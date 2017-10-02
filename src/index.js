4881c514b62a620083eaa1a44fda1669a5a17689
github username <github email address>
2017-10-02 14:47:19 -0400

    extracted all movie IDs array

--- src/index.js
+++ src/index.js
@@ -1,37 +1,50 @@
 const app = new App()
+const apiKey = "121f65c343fcbbfc19cceaf9c89e37fd"
 
 let pageMax
-let currentPage = 1
+let currentPage = 0
 let allIDs = []
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=121f65c343fcbbfc19cceaf9c89e37fd&language=en-US&page=${currentPage}`)
+  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
     .then(res => res.json())
-    .then(json => pageLooper(json))
-});
-  // Make a loop using the fetch request to grab all the IDs and place them into an array
-  // Randomly pick an ID from the array and render a preview on the page
-
-  function pageLooper(json){
-    pageMax = json.total_pages
-    let resultIDs = []
-    let jsonData
-    for (let i = 1; i < pageMax; i++){
-
-      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=121f65c343fcbbfc19cceaf9c89e37fd&language=en-US&page=${currentPage}`)
-        .then(res => res.json())
-        .then(json => )
-        })
-
-      for (let j = 0; j < jsonData.results.length; j++){
-        resultIDs.push(jsonData.results[j].id)
+    .then(json => {
+      pageMax = json.total_pages
+      return json
+    })
+    .then(() => {
+      for (let i = 0; i < pageMax; i++){
+        currentPage++
+        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=${currentPage}`)
+          .then(res => res.json())
+          .then(json => {
+            pageLooper(json)
+            pageMax = json.total_pages
+          })
       }
+    })
 
-      allIDs.push(resultIDs)
-      resultIDs = []
-      currentPage++
-    }
-    debugger
-    console.log(allIDs)
+
+
+});
+
+function pageLooper(json){
+
+  let resultIDs = []
+
+  for (let i = 0; i < json.results.length; i++){
+    resultIDs.push(json.results[i].id)
   }
+
+  allIDs.push(resultIDs)
+}
+
+function combineAllIDs(){
+  let newArray = []
+
+  for (let i = 0; i < allIDs.length; i++){
+    newArray = newArray.concat(allIDs[i]);
+  }
+  return newArray
+}
