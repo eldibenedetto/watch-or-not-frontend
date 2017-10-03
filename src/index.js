a61b31689da0cc551468f5d350b5ce6059217a7b
github username <github email address>
2017-10-03 14:18:46 -0400

    trailer renders

--- src/index.js
+++ src/index.js
@@ -1,59 +1,18 @@
 const app = new App()
-const apiKey = "121f65c343fcbbfc19cceaf9c89e37fd"
-
-let pageMax
-let currentPage = 0
-let allIDs = []
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
+  fetch('http://localhost:3000/api/v1/movies')
     .then(res => res.json())
-    .then(json => {
-      pageMax = json.total_pages
-      return json
-    })
-    .then(() => {
-      for (let i = 0; i < pageMax; i++){
-        currentPage++
-        fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=${currentPage}`)
-          .then(res => res.json())
-          .then(json => {
-            pageLooper(json)
-            pageMax = json.total_pages
-          })
-      }
-    })
-});
+    .then(res => render(res))
+})
 
-function pageLooper(json){
-
-  let resultIDs = []
-
-  for (let i = 0; i < json.results.length; i++){
-    resultIDs.push(json.results[i].id)
-  }
-  allIDs = allIDs.concat(resultIDs)
-}
-
-function cleanOutNoVideoTrailers(){
-  for (let i = 0; i < allIDs.length; i++){
-    let videoId = allIDs[i]
-    fetch(`https://api.themoviedb.org/3/movie/${videoId}?api_key=${apiKey}&append_to_response=videos`)
-      .then(res => res.json())
-      .then(res => {
-        if (res.videos.results.length === 0) {
-          allIDs.splice(i, 1)
-        }
-      })
-  }
-}
-
-
-function renderTrailer(){
-  let randomIndex = Math.floor(Math.random() * allIDs.length)
-  let videoID = allIDs[randomIndex]
-  fetch(`https://api.themoviedb.org/3/movie/${videoID}?api_key=${apiKey}&append_to_response=videos`)
-    .then(res => res.json())
-    .then(res => console.log(res))
+function render(res) {
+  let randomIndex = Math.floor(Math.random() * res.length)
+  let randomMovie = res[randomIndex]
+  let youtubeKey = randomMovie.youtube_key
+  let trailerUrl = `http://www.youtube.com/embed/${youtubeKey}?autoplay=1`
+  document.querySelector('#trailer').innerHTML = `<iframe width="420" height="315"
+src="${trailerUrl}">
+</iframe>`
 }
