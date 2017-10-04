170ca797541b079df3bdeb178000e47a2acfc193
github username <github email address>
2017-10-04 13:06:23 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -60,7 +60,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 document.querySelector('#watch').addEventListener("click", function(e){
   e.preventDefault()
-  console.log("clicked")
+  fetch("http://localhost:3000/api/v1/user_movies", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user_id: localStorage.userId,
+      movie_id: localStorage.movieId,
+      approved: 0
+    })
+  })
+  showTrailer()
+})
+
+document.querySelector('#nah').addEventListener("click", function(e){
+  e.preventDefault()
+  fetch("http://localhost:3000/api/v1/user_movies", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user_id: localStorage.userId,
+      movie_id: localStorage.movieId,
+      approved: 1
+    })
+  })
+  showTrailer()
 })
 
 // function fetchUsersWatchedMovies(){
@@ -77,9 +106,11 @@ function showTrailer(){
 function render(res) {
   let randomIndex = Math.floor(Math.random() * res.length)
   let randomMovie = res[randomIndex]
+    //add if statement to check if user has seen this trailer
   let youtubeKey = randomMovie.youtube_key
   let trailerUrl = `http://www.youtube.com/embed/${youtubeKey}?autoplay=1`
   document.querySelector('#trailer').innerHTML = `<h3>${randomMovie.title}</h3><iframe width="420" height="315"
   src="${trailerUrl}">
   </iframe>`
+  localStorage.setItem("movieId", `${randomMovie.id}`)
 }
