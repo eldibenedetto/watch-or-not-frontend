c85459be4c4ae1774430e1326457fc9b36330cdb
github username <github email address>
2017-10-05 13:46:12 -0400

    modals attached to posters

--- src/index.js
+++ src/index.js
@@ -107,7 +107,6 @@ function showTrailer(){
 function render(res) {
   let randomIndex = Math.floor(Math.random() * res.length)
   let randomMovie = res[randomIndex]
-    //add if statement to check if user has seen this trailer
   let youtubeKey = randomMovie.youtube_key
   let trailerUrl = `http://www.youtube.com/embed/${youtubeKey}?autoplay=1`
   document.querySelector('#mydivheader').innerHTML = `<h3><strong>${randomMovie.title}</strong></h3><iframe width="598" height="336"
@@ -120,51 +119,33 @@ function renderPosters(res) {
   let baseUrl = "https://image.tmdb.org/t/p/w640"
   res.movies.forEach(movie => {
   let fullUrl = baseUrl + JSON.parse(movie.raw_JSON).poster_path
-  document.querySelector("#moviePosters").innerHTML += `<img src=${fullUrl}>`
+  document.querySelector("#moviePosters").innerHTML += `<img id=${} src=${fullUrl}>`
   })
 }
 
-// //Make the DIV element draggagle:
-// dragElement(document.getElementById(("mydiv")));
-//
-// function dragElement(elmnt) {
-//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     /* if present, the header is where you move the DIV from:*/
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* otherwise, move the DIV from anywhere inside the DIV:*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-//
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-//
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//   }
-//
-//   function closeDragElement() {
-//     /* stop moving when mouse button is released:*/
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// base url for img src https://image.tmdb.org/t/p/w640/
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
