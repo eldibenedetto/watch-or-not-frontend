23b5c0844ea8f9d6333ff910e1cd640094a1fd82
github username <github email address>
2017-10-05 13:42:00 -0400

    add posters to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const app = new App()
-// let moviesUserWatched = []
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -26,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
       return res
     })
     .then(res => renderPosters(res))
-
 
     document.querySelector("#logInSignUp").style.display = "none"
     document.querySelector("#mainPage").style.display = "unset"
@@ -63,7 +61,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 })
 
-
 function enteringWatchZone(){
   fetch("http://localhost:3000/api/v1/user_movies", {
     method: "post",
@@ -76,7 +73,7 @@ function enteringWatchZone(){
       movie_id: localStorage.movieId,
       approved: 0
     })
-  })
+  }).then(res => res.json()).then(res => addPoster(res))
   showTrailer()
 }
 
@@ -96,16 +93,12 @@ function enteringNahZone(){
   showTrailer()
 }
 
-
 function showTrailer(){
   let username = localStorage.userName
   fetch(`http://localhost:3000/api/v1/movies?username=${username}`)
     .then(res => res.json())
     .then(res => render(res))
-    .then(res => {debugger})
-
 }
-
 
 function render(res) {
   let randomIndex = Math.floor(Math.random() * res.length)
@@ -126,47 +119,8 @@ function renderPosters(res) {
   })
 }
 
-// //Make the DIV element draggagle:
-// dragElement(document.getElementById(("mydiv")));
-//
-// function dragElement(elmnt) {
-//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     /* if present, the header is where you move the DIV from:*/
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* otherwise, move the DIV from anywhere inside the DIV:*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-//
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-//
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//   }
-//
-//   function closeDragElement() {
-//     /* stop moving when mouse button is released:*/
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
-
-
-// base url for img src https://image.tmdb.org/t/p/w640/
+function addPoster(res){
+  let baseUrl = "https://image.tmdb.org/t/p/w640"
+  let fullUrl = baseUrl + JSON.parse(res.raw_JSON).poster_path
+  document.querySelector("#moviePosters").innerHTML += `<img src=${fullUrl}>`
+}
